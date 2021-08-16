9c84a4ace4fe673d2e4db3e3c48428e7346db33b
Eingest <79462227+Eingest@users.noreply.github.com>
2021-08-16 14:48:09 +0200

    august 16.

--- src/pages/NewMeetup.tsx
+++ src/pages/NewMeetup.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  function addMeetupHandler(meetupData: any) {
+    fetch(
+      "https://react-getting-started-1b898-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
